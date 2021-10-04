bf77016f6c39e9909605b6fb53fae527d0dc0ea3
lawrence-liu <lawrence_liu@trendmicro.com>
2021-10-04 18:14:31 +0800

    updte file for button usage

--- button.py
+++ button.py
@@ -1,5 +1,18 @@
 import RPi.GPIO as GPIO
 import time
+from audio import AudioFile 
+from datetime import datetime
+import os
+import sys
+
+def record_n_play():
+    stamp = str(int(datetime.utcnow().timestamp()))
+    filename = f"{stamp}.wav"
+    a = AudioFile(filename)
+    a.record(sec=10)
+    a.play_last_nine()
+    a.play()
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.IN)
 
@@ -8,3 +21,4 @@ while True:
     if inputValue == False:
         print("Button pressed")
         time.sleep(0.3)
+        record_n_play()
